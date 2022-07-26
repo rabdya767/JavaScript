9b415aa3242f109471d41540d579b2677395633b
Rabdya Santhosh <santhoshr.be22@uceou.edu>
2022-07-26 09:16:49 +0530

    Update Clock

--- clock.js
+++ clock.js
@@ -4,10 +4,16 @@ const tick = () =>{
 
     const now = new Date();
 
-    const h=now.getHours();
-    const m=now.getMinutes();
-    const s=now.getSeconds();
-
+    let h=now.getHours();
+    let m=now.getMinutes();
+    let s=now.getSeconds();
+    h=String(h).padStart(2, '0');
+    m=String(m).padStart(2,'0');
+    s=String(s).padStart(2,'0');
+    let ampm="PM";
+    if(13<h<23){
+        ampm="AM";
+    }
     // console.log(`${h}:${m}:${s}`);
     const html = `<h1>
                  <span>${h}</span>
@@ -15,6 +21,8 @@ const tick = () =>{
                  <span>${m}</span>
                  :
                  <span>${s}</span>
+                 
+                 <span>${ampm}</span>
                  </h1>`;
                 
     clock.innerHTML = html;

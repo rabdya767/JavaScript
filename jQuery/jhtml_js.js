2e4fd63fd039d17ff8ce5256ba24a033eff2c671
Rabdya Santhosh <santhoshr.be22@uceou.edu>
2022-08-07 10:01:45 +0530

    jQuery examples (#3)

---
+++ jQuery/jhtml_js.js
@@ -0,0 +1,7 @@
+// const myElement = document.getElementById("123");
+// myElement.textContent = "Hello Rabdyaa!";
+
+$(document).ready(function () {
+    var myElement = $("#01");
+    myElement.text("Hello Rabdya!");
+});
